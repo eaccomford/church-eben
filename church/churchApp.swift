5476534952ffda5a1cb65741202f905d8aa68403
Emmanuel Accomford <eaccomford@gmail.com>
2021-02-05 00:35:27 +0000

    Initial Commit

---
+++ church/churchApp.swift
@@ -0,0 +1,17 @@
+//
+//  churchApp.swift
+//  church
+//
+//  Created by PK on 05/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct churchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
